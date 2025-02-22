658acaf209657c665a6d5b40a56d3824af41cdbb
Pradeep <pawarpradeep1707@gmail.com>
2025-02-22 22:02:07 +0530

    Added a smiley emoji URL

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +7,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+          child: Lottie.network(
+              'https://lottie.host/31b0c1dc-5ff1-45df-b96c-fa1200c8c554/QTcdtW0332.json')),
     );
   }
 }
